1e53fa3cee15b8b1317194a383a587d68501ccfe
Vishal Kushwaha <vishukushwaha60@gmail.com>
2026-05-09 06:58:54 -0300

    FlateList

---
+++ src/Component/FlateList.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View ,FlatList} from 'react-native'
+import React from 'react'
+
+const FlateList = () => {
+  return (
+    <View>
+      <Text>FlateList</Text>
+    </View>
+  )
+}
+
+export default FlateList
+
+const styles = StyleSheet.create({})
