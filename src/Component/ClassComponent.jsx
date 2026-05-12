601c59ce8ed01bf5a6f67448a733d0dba4ea53e1
Vishal Kushwaha <vishukushwaha60@gmail.com>
2026-05-12 07:26:35 -0300

    class besed component

---
+++ src/Component/ClassComponent.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Component } from "react";
+import {Text,Button,State} from 'react-native'
+
+class ClassComponent extends Component {
+    //always define state in constructor method
+    constructor(){
+        super()
+        this.state={
+            myname:'Vishal'
+        }
+    }
+    updateName=()=>{
+        this.setState({myname:'Kushwaha'})
+    }
+    render() {
+         return(
+            <View>
+                <Text style={{fontSize:30}}>Name:{this.state.myname}</Text>
+                <Button />
+            </View>  
+         )
+    }
+}
