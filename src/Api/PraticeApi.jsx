d650c867f2749825d9a9f9a23e994297c7ce64a6
Vishal Kushwaha <vishukushwaha60@gmail.com>
2026-05-18 12:00:25 -0300

    Practice Api

---
+++ src/Api/PraticeApi.jsx
@@ -0,0 +1,18 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PraticeApi = () => {
+  return (
+    <View>
+
+      <Text>PraticeApi</Text>
+      <Pressable>
+        <Text>Fatch Data</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+export default PraticeApi
+
+const styles = StyleSheet.create({})
