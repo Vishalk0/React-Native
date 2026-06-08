d4dcd07155bf66a0cfed86afb31dd6abd8ca211b
Vishal Kushwaha <vishukushwaha60@gmail.com>
2026-06-08 09:26:52 -0300

    post

---
+++ src/Api/Post_API.jsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View,TextInput,Button } from 'react-native'
+import React, { useState } from 'react'
+
+
+
+const Post_API = () => {
+    const[data,setData]=useState({id:'',name:'',age:''})
+    const handleApi=(field,value)=>{
+        setData({...data,[field]:value})
+    }
+
+  return (
+    <View>
+      <Text style={{fontSize:24,}}>Post_API</Text>
+      <TextInput
+      placeholder='Enter Your ID'
+      />
+      <TextInput
+      placeholder='Enter Your Name'/>
+      <TextInput
+      placeholder='Enter Your Age'
+      />
+      <Button title='Submit'/>
+    </View>
+  )
+}
+
+export default Post_API
+
+const styles = StyleSheet.create({})
