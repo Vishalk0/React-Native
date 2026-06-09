18301062bc1754c9599c40bb32ea1a7f2b83fb62
Vishal Kushwaha <vishukushwaha60@gmail.com>
2026-06-09 12:17:38 -0300

    done.

--- src/Api/Post_API.jsx
+++ src/Api/Post_API.jsx
@@ -1,12 +1,24 @@
-import { StyleSheet, Text, View,TextInput,Button } from 'react-native'
+import { StyleSheet, Text, View,TextInput,Button, Alert } from 'react-native'
 import React, { useState } from 'react'
+import { Axios } from 'axios'
 
 
 
 const Post_API = () => {
     const[data,setData]=useState({id:'',name:'',age:''})
+    //this line is create to handle the field and value  
     const handleApi=(field,value)=>{
         setData({...data,[field]:value})
+    }
+    //when we are used api then we follow try catch method 
+    const handleSubmit= async()=>{
+      try {
+        const response= await Axios.post('url')
+        Alert.alert('Success!', 'Data submit successfully')
+        setData()
+      } catch (error) {
+        Alert.alert('ERROR','Message are not Submitted...')
+      }
     }
 
   return (
@@ -14,11 +26,17 @@ const Post_API = () => {
       <Text style={{fontSize:24,}}>Post_API</Text>
       <TextInput
       placeholder='Enter Your ID'
+      value={data.id}
+      onChangeText={(value)=>handleApi('id',value)}
       />
       <TextInput
+      value={data.name}
       placeholder='Enter Your Name'/>
+      onChangeText={(value)=>handleApi('name',value)}
       <TextInput
+      value={data.age}
       placeholder='Enter Your Age'
+      onChangeText={(value)=> handleApi('age',value)}
       />
       <TextInput
       placeholder='Enter Your Profession'
