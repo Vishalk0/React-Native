0a45efd0a71501086fe863b500b3de3c9a905d92
Vishal Kushwaha <vishukushwaha60@gmail.com>
2026-07-14 06:29:10 -0300

    update code

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Button } from 'react-native/types_generated/index'
 
 const About = (props) => {
     const{username}=props.route.params
   return (
     <View>
       <Text>Welcome {username}</Text>
+      <Button title="Go Back" onPress={()=>props.navigation.goBack()}  />
     </View>
   )
 }
--- src/Component/navigationPractice.jsx
+++ src/Component/navigationPractice.jsx
@@ -6,24 +6,39 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 const navigationPractice = () => {
   return (
     <NavigationContainer>
-      <stack.Navigator>
-        <stack.Screen name="Home" component={Home}/>
-        <stack.Screen name="Contact" component={Contact}/>
+      <stack.Navigator
+      //this is used to implement style in all screen when we place one place
+        screenOptions={{
+
+      }}
+      >
+        <stack.Screen name="Home" component={Home}
+         //this is used to styling only one header screen.
+         options={{
+            title: 'Home Screen',
+             headerTintColor: 'red', 
+             headerTitleStyle: {
+
+            }
+          }}
+        />
+
+        <stack.Screen name="Contact" component={Contact} />
       </stack.Navigator>
 
     </NavigationContainer>
-    
+
   )
 }
 //we are create instance for navigation purpuse 
-const stack=createNativeStackNavigator()
+const stack = createNativeStackNavigator()
 
 export default navigationPractice
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        
-    },
+  container: {
+    flex: 1,
+
+  },
 
 })
